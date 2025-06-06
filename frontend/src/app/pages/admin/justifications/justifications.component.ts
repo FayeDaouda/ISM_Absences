eb3927f50618365d7d79e85e8944ef3287d7babf
malayed12 <laminelayed@gmail.com>
2025-06-06 13:20:41 +0000

    réglage du défaut de la page de justification

--- frontend/src/app/pages/admin/justifications/justifications.component.ts
+++ frontend/src/app/pages/admin/justifications/justifications.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Absence, AbsenceService } from '../../../shared/services/impl/absence.service';
 
 @Component({
-  selector: 'app-justifications',
+  selector: 'app-justification',
   templateUrl: './justifications.component.html',
 })
 export class JustificationComponent implements OnInit {
   absenceId!: string;
+  absence!: Absence | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private absenceService: AbsenceService
+  ) {}
 
   ngOnInit(): void {
     this.absenceId = this.route.snapshot.paramMap.get('id')!;
+    this.absenceService.getAbsences().subscribe((absences) => {
+      this.absence = absences.find((a) => a.id === this.absenceId);
+    });
   }
 }
